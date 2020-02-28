2db7a5b30ede8780b57119ed83e74af3a9872e91
bsalman <bsalman395850@gmail.com>
2020-02-29 00:18:23 +0100

    vetig

---
+++ js-function -spilt-method-to-change-string-to-array/js/index.js
@@ -0,0 +1,9 @@
+let str ='Ahmad'
+//Ich muss diese Worte zu einem Array ändern und die rückwärts  schreiben.
+
+let arr = []
+function reverseSpiltString(string) {
+    return arr = string.split('').reverse().join('')
+    
+}
+document.write(reverseSpiltString(str))
