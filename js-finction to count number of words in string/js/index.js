7239ef312f1f2272e2f745c6e07ae22210eccd8b
bsalman <bsalman395850@gmail.com>
2020-03-02 10:39:35 +0100

    vertig

---
+++ js-finction to count number of words in string/js/index.js
@@ -0,0 +1,12 @@
+let str= 'hello world hih'
+
+
+function wordeCounter(txt) {
+    let st = txt.split(' ') 
+    console.log(st.length)
+    // let counter = 0
+    // let i = txt.indexOf(' ')
+
+
+}
+wordeCounter(str)
