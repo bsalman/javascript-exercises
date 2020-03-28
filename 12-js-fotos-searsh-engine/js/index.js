a685400c4cb96b20c1aee64b309d31b76475bab2
bsalman <bsalman395850@gmail.com>
2020-03-28 15:06:43 +0100

    28.3.2020

--- 12-js-fotos-searsh-engine/js/index.js
+++ 12-js-fotos-searsh-engine/js/index.js
@@ -27,13 +27,43 @@ function creatElement(divId) {
     let searshInput=document.createElement('input')
     searshInput.setAttribute('type','text')
     searshInput.setAttribute('placeholder','search for ')
+//creating color select list 
+    let colorList =document.createElement('select')
+    let firstColorOpt=  document.createElement('option') 
+        firstColorOpt.innerText='select color'
+        firstColorOpt.setAttribute('value','')
+        colorList.append(firstColorOpt)
+        //creating colorlist Options using forEach after creating colorArr
+    let colorArr=["grayscale", "transparent", "red", "orange", "yellow", "green", "turquoise", "blue", "lilac", "pink", "white", "gray", "black", "brown"]
+        colorArr.forEach(element => {
+    let colorOpt=  document.createElement('option') 
+        colorOpt.innerText=element
+        colorOpt.setAttribute('value',element)
+        colorList.append(colorOpt)
+    });
+//creating catigoryList 
+    let categoryList = document.createElement('select')
+    let firstcategoryOpt=document.createElement('option')
+        firstcategoryOpt.innerText='select Category'
+        firstcategoryOpt.setAttribute('value','')
+        categoryList.append(firstcategoryOpt)
+
+     let categoryArr=['backgrounds', 'fashion', 'nature', 'science', 'education', 'feelings', 'health', 'people', 'religion', 'places', 'animals', 'industry', 'computer', 'food', 'sports', 'transportation', 'travel', 'buildings', 'business', 'music' ]   
+         categoryArr.forEach(element => {
+         categoryOpt=document.createElement('option')
+         categoryOpt.innerText=element
+         categoryOpt.setAttribute('value',element)
+         categoryList.append(categoryOpt)
+     }); 
 
 //creating searchBtn
-    let searchBtn=document.createElement('button')
+    let searchBtn =document.createElement('button')
     searchBtn.innerText = 'search'
 
     
     div1.append(searshInput)
+    div1.append(colorList)
+    div1.append(categoryList)
     div1.append(searchBtn)
     div1.append(div3)
     containerDiv.append(div1)
@@ -41,18 +71,33 @@ function creatElement(divId) {
 
 searchBtn.addEventListener('click',function (e) {
   let keyWord=searshInput.value
-    //calling fotoSearch
-    fotoSearch(keyWord,div2,div3,1)
+   
+   //calling fotoSearch
+    fotoSearch(keyWord,div2,div3,1,colorList,categoryList)
 
     
 })
     
 }
-async function fotoSearch(keyWord,imgDivContainer,btncontainer,pageNume) {
+async function fotoSearch(keyWord,imgDivContainer,btnContainer,pageNume,colorElement,categoryElement) {
+ // i searched in googel for this quastion (Get selected value in dropdown list using JavaScript) to get this result   
+
+ 
+    let selectedColor=colorElement.options[colorElement.selectedIndex].value
+    let colorParameter=''
+       if (selectedColor){
+         colorParameter='&colors='+selectedColor
+       }
+
+    let selesctedCategory= categoryElement.options[colorElement.selectedIndex].value
+    let categoryParameter=''
+    if(selesctedCategory){
+        categoryParameter='&category'+selesctedCategory
+    }
 
 
 //geting data using fetch 
-let response =await fetch('https://pixabay.com/api/?key=12000491-41fc68d8c365df909e022ceb6&q='+keyWord+'&page='+pageNume)
+let response =await fetch('https://pixabay.com/api/?key=12000491-41fc68d8c365df909e022ceb6&q='+keyWord+'&page='+pageNume+colorParameter+categoryParameter)
 if (response.status==200){
     let data =await response.json()
     console.log(data);
@@ -60,18 +105,19 @@ if (response.status==200){
     let num = data.totalHits /20
     let btnNumber= Math.ceil(num)
     // console.log(btnNumber);
-    btncontainer.innerHTML=''
+    btnContainer.innerHTML=''
     imgDivContainer.innerHTML=''
     
     for (let i = 0; i < btnNumber ; i++) {
         let pagBtn =document.createElement('button')
         pagBtn.innerText='page'+(i+1)
-        btncontainer.append(pagBtn)
+        btnContainer.append(pagBtn)
 //to mack the page non klickapel   
     if (pageNume== i+1){pagBtn.disabled=true}
 //calling the seame function evrytime the user klick one of page Buttons
-     pagBtn = document.addEventListener('click',function () {
-     fotoSearch(keyWord,imgDivContainer,btncontainer,i+1)
+        
+     pagBtn.addEventListener('click',function () {
+     fotoSearch(keyWord,imgDivContainer,btnContainer,i+1,colorElement,categoryElement)
     })
     }
  console.log(data.hits);
